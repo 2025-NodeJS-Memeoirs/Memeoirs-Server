cfe57e06d38bd7a6347a47d11ea56b77b5be6c3f
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-06-12 11:23:33 +0900

    Feat: 유저 테이블들 생성

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const { sequelize } = require('./models/index'); 
+
+// 서버 시작 전 테이블 동기화
+sequelize.sync({ force: true })  // force: false로 설정해서 기존 테이블 덮어쓰지 않음, (true로 해서 테이블이 잘 생성되는지 확인해봄)
+  .then(() => {
+    console.log('디비와 테이블 동기화 완료');
+  })
+  .catch((error) => {
+    console.error('디비와 테이블 동기화 실패:', error);
+  });
+
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
 });
---
+++ models/index.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+});
+
+const UserLogin = require('./UserLogin')(sequelize);
+const UserSignin = require('./UserSignin')(sequelize);
+
+const models = { sequelize, UserLogin, UserSignin };
+
+// 관계 설정 호출 (여기서 associate 실행)
+Object.values(models).forEach(model => {
+  if (typeof model.associate === 'function') {
+    model.associate(models);
+  }
+});
+
+module.exports = models;

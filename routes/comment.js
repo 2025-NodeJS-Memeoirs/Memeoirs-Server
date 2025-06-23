b9b8883a1f08992538db69a77a3672f1b207718e
subin1848 <s2315@e-mirim.hs.kr>
2025-06-24 08:35:44 +0900

    ✨ [feat] 댓글 작성 라우터

--- routes/comment.js
+++ routes/comment.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const authenticateToken = require('../middlewares/authenticateToken');  //jwt 토큰 import 
 
 const commentController = require('../controllers/commentController');
 
 // 게시글과 댓글, 작성자 정보 조회
 router.get('/:id', commentController.getDiaryWithCommments);
+// 댓글 작성
+router.post('/', authenticateToken, commentController.createComment);
 
 module.exports = router;
